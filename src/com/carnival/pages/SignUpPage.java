16343ed5bfc8bb742c1bf3f1d3eda35a8d2ef353
Himanshu Arora <Himanshu.arora23@hotmail.com>
2021-01-27 01:37:54 +0530

    ALL

--- src/com/carnival/pages/SignUpPage.java
+++ src/com/carnival/pages/SignUpPage.java
@@ -6,11 +6,7 @@ import com.carnival.base.CreateDriver;
 
 public class SignUpPage {
 
-	public SignUpPage(ChromeDriver driver) {
-		CreateDriver.driver=driver;
 	
-		
-	}
 
 	public void ClickOnCreateAccount() {
 		// TODO Auto-generated method stub
--- src/com/carnival/pages/WelcomePage.java
+++ src/com/carnival/pages/WelcomePage.java
@@ -11,36 +11,36 @@ import com.carnival.base.CreateDriver;
 
 public class WelcomePage extends CreateDriver {
 	
-	public WelcomePage(WebDriver driver) {
-		CreateDriver.driver=driver;
 	
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
-
-	public void ExploreLink() {
-		act.moveToElement(driver.findElement(rb.getString("Explore"))).build().perform();
-	}
-	public void DestinationLink(){
-		driver.findElement(By.xpath("//span[text()='Destinations']")).click();
-	}
-	public void CarribeanLink()
-{
-		act.moveToElement(driver.findElement(rb.getString("Caribbean"))).build().perform();
-	}
-	public void StartExploring()
-	{
-			driver.findElement(rb.getString("StartExploring")).click();
-		}
-	public void Sailings() {
-
-		driver.findElement(rb.getString("Sailings")).click();
-	}
-	public void SailingsClick() {
-
-		act.moveToElement(driver.findElement("//div[@class='menu-wrap']/ul/li[3]/a")).click().perform();
-	}
-		
-		
-	}
-
-  
